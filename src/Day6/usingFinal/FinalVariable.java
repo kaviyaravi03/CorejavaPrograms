9d6e2232eef906de380b71389a17be47a8e94eb5
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-02 20:21:00 +0530

    commit

--- src/Day6/usingFinal/FinalVariable.java
+++ src/Day6/usingFinal/FinalVariable.java
@@ -1,19 +1,34 @@
 package Day6.usingFinal;
 
 public class FinalVariable {
-	void show() {
-		System.out.println("Final class cannot be inherited");
-	}
-}
 
-//can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
-class FinalChildClass extends FinalClass {
-}
+	// final int x; // final instance variable must be initialized
 
-public class FinalClassDemo {
-	public static void main(String[] args) {
-		// Create the object of final class
-		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
-		f1.show();
+	final int x = 100;
+
+	// Declare a static blank final variable.
+	final static int Y;
+
+	// Declare & intialize static final variable.
+	final static int Z = 10;
+
+	// instatnce method
+	void change() {
+		x = 30; // final variables can't be reassigned
+		Y = 200; // final static variables can't be reassigned
 	}
+
+	@Override
+	public String toString() {
+		return "FinalVariable [x=" + x + ", Y" + Y + "]";
+	}
+
+	// Declare a static block to initialize the final static variable.
+	static {
+		Y = 20;
+		Z = 100; // Once intialized can't be reassigned
+		System.out.println("Value of Y: " + Y);
+	}
+
+	
 }
