946090bf759784e8dddd36b9f424fbd608ab51d3
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-02 16:37:37 +0530

    commit

---
+++ src/day5/multilevelinheritance/texteditor/Notepad.java
@@ -0,0 +1,32 @@
+package day5.multilevelinheritance.texteditor;
+
+public class Notepad {
+
+	
+		// TODO Auto-generated method stub
+		 protected String content;
+
+		    public Notepad() {
+		        this.content = "";
+		    }
+
+		    public void write(String text) {
+		        content += text; //content = content+text
+		    }
+
+		    public void displayContent() {
+		        System.out.println("Notepad Content: " + content);
+		    }
+		    
+		    
+		    /*
+		     * returntype funcName()
+		     * {
+		     * 		//statement
+		     * }
+		     * 
+		     * */
+
+	}
+
+
