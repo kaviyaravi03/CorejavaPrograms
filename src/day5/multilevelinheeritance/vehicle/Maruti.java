87047ed2f5fb1398c0a63681c628baa2da86fe57
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-02 19:09:04 +0530

    commit

---
+++ src/day5/multilevelinheeritance/vehicle/Maruti.java
@@ -0,0 +1,17 @@
+package day5.multilevelinheeritance.vehicle;
+
+public class Maruti {
+
+	public Maruti() {
+		System.out.println("Class Maruti");
+	}
+
+	public void brand() {
+		System.out.println("Brand: Maruti");
+	}
+	public void speed() {
+		System.out.println("Max: 90Kmph");
+	}
+
+
+}
