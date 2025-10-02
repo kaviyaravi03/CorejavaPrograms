37390966fbf14e21ab8da47089c6297827c2ef9e
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-02 19:07:37 +0530

    commit

---
+++ src/day5/multilevelinheeritance/vehicle/Car.java
@@ -0,0 +1,14 @@
+package day5.multilevelinheeritance.vehicle;
+
+public class Car {
+	// TODO Auto-generated method stub
+			public Car() {
+				System.out.println("Class Car");
+			}
+
+			public void vehicleType() {
+				System.out.println("Vehicle Type: Car");
+			}
+			
+
+}
