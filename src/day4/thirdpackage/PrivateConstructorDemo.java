b3486ddef8623210c9c80b6471964b92c00dd913
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-02 16:21:58 +0530

    commit

--- src/day4/thirdpackage/PrivateConstructorDemo.java
+++ src/day4/thirdpackage/PrivateConstructorDemo.java
@@ -1,11 +1,9 @@
-//Program to demonstrate Private Constructor and Singleton class
-package com.tnsif.dayfour.thirdpackage;
+package day4.thirdpackage;
 
 public class PrivateConstructorDemo {
-	public static void main(String s[]) {
-		// Can't create object with private constructor
-		// MyClass m1=new MyClass();
-		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		MyClass m=MyClass.getObject();
 		m.setId(10);
 		
@@ -14,5 +12,7 @@ public class PrivateConstructorDemo {
 		System.out.println(m);
 		System.out.println(m1);
 		
+
 	}
+
 }
