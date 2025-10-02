b60faea828d39f4d5e6ce76acd445ce7f80864a4
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-02 17:13:56 +0530

    commit

---
+++ src/vehicle/Maruti800.java
@@ -0,0 +1,13 @@
+package vehicle;
+
+public class Maruti800 {
+
+	public Maruti800() {
+		System.out.println("Maruti Model: 800");
+	}
+
+	public void speed() {
+		System.out.println("Max: 80Kmph");
+	}
+
+}
