0e886d77a2c82e2af5f83bb4bc4d3277d9887016
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-04 22:30:24 +0530

    commit

---
+++ src/Day7/overloading/v1/Executor.java
@@ -0,0 +1,10 @@
+package Day7.overloading.v1;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Day7/overloading/v1/Operations.java
@@ -0,0 +1,10 @@
+package Day7.overloading.v1;
+
+public class Operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
