74ba08e1d1b8057532206d84598ba407fa5bd343
Kaviya R <kaviyaravi0021@gmail.com>
2025-10-27 11:43:51 +0530

    commit

---
+++ src/Day12/InvalidMarksException.java
@@ -0,0 +1,13 @@
+package Day12;
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
